6ed2a2dd05412e78e755c4a6c3fb9e51033b090e
Arda Meçik <mecikarda@gmail.com>
2026-06-20 16:44:19 +0300

    fix: tulpar.py flake8 E402 hatasi duzeltildi

--- tulpar.py
+++ tulpar.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from tulpar.main import ana_fonksiyon
+from tulpar.main import ana_fonksiyon  # noqa: E402
 
 if __name__ == "__main__":
     ana_fonksiyon()
